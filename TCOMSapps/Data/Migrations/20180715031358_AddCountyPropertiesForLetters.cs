080f59ad43ec129c249839fef455e7c3786558bf
Mitchell Wilson <mwilson@nassautaxes.com>
2018-07-15 00:34:49 -0400

    Add County Props for letters

---
+++ TCOMSapps/Data/Migrations/20180715031358_AddCountyPropertiesForLetters.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace TCOMSapps.Data.Migrations
+{
+    public partial class AddCountyPropertiesForLetters : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "OosTitleLetterAddressLine1",
+                table: "Counties",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "OosTitleLetterAddressLine2",
+                table: "Counties",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "OosTitleLetterCity",
+                table: "Counties",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "OosTitleLetterPhoneNumbers",
+                table: "Counties",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "OosTitleLetterState",
+                table: "Counties",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "OosTitleLetterTaxCollectorName",
+                table: "Counties",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "OosTitleLetterWebsite",
+                table: "Counties",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "OosTitleLetterZip",
+                table: "Counties",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "OosTitleLetterAddressLine1",
+                table: "Counties");
+
+            migrationBuilder.DropColumn(
+                name: "OosTitleLetterAddressLine2",
+                table: "Counties");
+
+            migrationBuilder.DropColumn(
+                name: "OosTitleLetterCity",
+                table: "Counties");
+
+            migrationBuilder.DropColumn(
+                name: "OosTitleLetterPhoneNumbers",
+                table: "Counties");
+
+            migrationBuilder.DropColumn(
+                name: "OosTitleLetterState",
+                table: "Counties");
+
+            migrationBuilder.DropColumn(
+                name: "OosTitleLetterTaxCollectorName",
+                table: "Counties");
+
+            migrationBuilder.DropColumn(
+                name: "OosTitleLetterWebsite",
+                table: "Counties");
+
+            migrationBuilder.DropColumn(
+                name: "OosTitleLetterZip",
+                table: "Counties");
+        }
+    }
+}
--- TCOMSapps/Features/OOSTitles/entities/County.cs
+++ TCOMSapps/Features/OOSTitles/entities/County.cs
@@ -3,22 +3,37 @@
 
 namespace TCOMSapps.Features.OOSTitles.entities
 {
-    public class County
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string PocUserId { get; set; }
-        public bool SharedData { get; set; }
-        public bool Active { get; set; }
-        public int DefaultLocation { get; set; }
-        public int Theme { get; set; }
-       
-        public List<Location> Locations { get; set; }
+  public class County
+  {
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string PocUserId { get; set; }
+    public bool SharedData { get; set; }
+    public bool Active { get; set; }
+    public int DefaultLocation { get; set; }
+    public int Theme { get; set; }
 
-        public string OosTitleEmailAddress { get; set; }
+    public List<Location> Locations { get; set; }
 
-        public string OosTitlesCopyEmailAddress { get; set; }
+    public string OosTitleEmailAddress { get; set; }
 
+    public string OosTitlesCopyEmailAddress { get; set; }
 
-    }
+    public string OosTitleLetterAddressLine1 { get; set; }
+
+    public string OosTitleLetterAddressLine2 { get; set; }
+
+    public string OosTitleLetterCity { get; set; }
+
+    public string OosTitleLetterState { get; set; }
+
+    public string OosTitleLetterZip { get; set; }
+
+    public string OosTitleLetterPhoneNumbers { get; set; }
+
+    public string OosTitleLetterTaxCollectorName { get; set; }
+
+    public string OosTitleLetterWebsite { get; set; }
+
+  }
 }
