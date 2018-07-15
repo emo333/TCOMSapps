f6cdf637059dd79cd2ce575b26fa86379273d97f
Mitchell Wilson <mwilson@nassautaxes.com>
2018-07-15 00:34:49 -0400

    Add County Props for letters

--- TCOMSapps/Data/Migrations/20180715031358_AddCountyPropertiesForLetters.Designer.cs
+++ TCOMSapps/Data/Migrations/20180715031358_AddCountyPropertiesForLetters.Designer.cs
@@ -12,9 +12,10 @@ using TCOMSapps.Features.OOSTitles.entities;
 namespace TCOMSapps.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180715031358_AddCountyPropertiesForLetters")]
+    partial class AddCountyPropertiesForLetters
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
